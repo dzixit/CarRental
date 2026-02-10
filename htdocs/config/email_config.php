710192c0e78c3aa878bd00fdb3fd8295dda49ff0
Konrad Pajor <156487989+dzixit@users.noreply.github.com>
2026-02-10 17:41:28 +0100

    Clear email configuration values for security

--- htdocs/config/email_config.php
+++ htdocs/config/email_config.php
@@ -2,10 +2,10 @@
 // Konfiguracja email
 define('SMTP_HOST', 'smtp.gmail.com');
 define('SMTP_PORT', 465);
-define('SMTP_USERNAME', 'konradprojektowyuz@gmail.com'); // Zastąp swoim emailem
-define('SMTP_PASSWORD', 'ioxn sugx jcih eijb'); // Zastąp hasłem aplikacji
-define('SMTP_FROM', 'konradprojektowyuz@gmail.com');
+define('SMTP_USERNAME', ''); // Zastąp swoim emailem
+define('SMTP_PASSWORD', ''); // Zastąp hasłem aplikacji
+define('SMTP_FROM', '');
 define('SMTP_FROM_NAME', 'Wypozyczalnia Samochodow');
-define('BASE_URL', 'https://carrentaluz.rf.gd/'); // Zastąp swoją domeną
+define('BASE_URL', '/'); // Zastąp swoją domeną
 define('SMTP_ALT_SECURE', 'ssl');
 ?>
